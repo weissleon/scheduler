41fffbfeb6954c9951967f1ac8fdfdee0346bb77
weissleon <weissleon1943@gmail.com>
2021-08-26 10:34:08 +0900

    Update page for temporary display

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,34 @@
+import { getSchedules } from "@util/ScheduleManager";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-// import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-  return <main>Hello World</main>;
+  const [schedules, setSchedules] = useState([]);
+  useEffect(() => {
+    (async () => {
+      setSchedules(await getSchedules());
+    })();
+
+    return () => {};
+  }, []);
+
+  return (
+    <main>
+      {schedules.length > 0 &&
+        schedules.map((schedule) => {
+          return (
+            <div style={{ background: "#ccc" }}>
+              <p>{schedule.id}</p>
+              <p>{schedule.status}</p>
+              <p>{schedule.creator}</p>
+              <p>{schedule.participants}</p>
+              <p>{schedule.title}</p>
+              <p>{schedule.detail}</p>
+            </div>
+          );
+        })}
+    </main>
+  );
 };
 
 export default Home;
