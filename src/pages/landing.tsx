321e3a133c12d9c9284d88deba5bed0d59c024c9
weissleon <weissleon1943@gmail.com>
2021-10-06 17:45:10 +0900

    Update landing page

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,11 +1,13 @@
 import { NextPage } from "next";
 import Link from "next/link";
+import ScheduleImage from "../../public/svg/first_block.svg";
+import Image from "next/image";
 
 const Landing: NextPage = () => {
   return (
-    <main className="flex flex-col min-h-screen bg-red-50 ">
+    <main className="flex flex-col min-h-screen px-60 bg-red-50 ">
       {/* Appbar */}
-      <div className="grid w-screen grid-cols-3 px-12 h-28 bg-red-50">
+      <div className="grid w-full grid-cols-3 h-28 bg-red-50">
         {/* LOGO */}
         <div className="flex items-center w-12 ">
           <Link href="/landing">
@@ -30,10 +32,26 @@ const Landing: NextPage = () => {
             <a>Log in</a>
           </Link>
           <Link href="/sign_up">
-            <a className="px-10 py-4 font-bold bg-yellow-200 rounded-lg shadow-sm">
+            <a className="px-10 py-4 font-bold bg-yellow-300 rounded-lg shadow-sm">
               Sign up
             </a>
           </Link>
+        </div>
+      </div>
+      {/* First Content Block */}
+      <div
+        className="grid grid-flow-col p-16"
+        style={{ gridTemplateColumns: "2fr 3fr" }}
+      >
+        {/* Text Area */}
+        <div className="flex items-center w-full">
+          <h1 className="font-extrabold leading-normal text-gray-800 text-8xl">
+            Schedule your day with your friends.
+          </h1>
+        </div>
+        {/* Image Area */}
+        <div className="justify-self-end">
+          <Image src={ScheduleImage} height={1000} width={1200} />
         </div>
       </div>
     </main>
